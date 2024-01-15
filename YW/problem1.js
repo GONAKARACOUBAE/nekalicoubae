464dd5b10016127ab61436075f955a280dd1e631
Dominick <dominick.kim@peopet.co.kr>
2024-01-15 19:05:51 +0900

    [FEAT] solution problem 1 2minutes

--- YW/problem1.js
+++ YW/problem1.js
@@ -1,1 +1,11 @@
 // 오늘의 문제 풀이 1번
+const solution = (arr) => {
+  let answer = [];
+  arr.map((number, index) => {
+    if (answer[answer.length - 1] !== number) {
+      answer.push(number);
+    }
+  });
+
+  return answer;
+};
