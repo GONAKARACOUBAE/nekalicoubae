5fc314771670804ea027bf29e96dc4cd1de0d187
Andult <dksjj123@gmail.com>
2024-01-15 22:11:52 +0900

    Update samenumber.py
    리스트 스플릿?하는 기능 배우기

--- JY/samenumber.py
+++ JY/samenumber.py
@@ -5,7 +5,7 @@ ch=0
 ans=[]
 bef=None
 now=0
-for i in range(1,length,2) :
+for i in range(1,length,2) : # 다른 방식 찾기
     now=int(num_list[i])
     if bef!=now :
         ch=0
@@ -13,6 +13,6 @@ for i in range(1,length,2) :
         ans.append(now)
         ch=1
     bef=now
-print(ans)
+solution(ans)
 
 
