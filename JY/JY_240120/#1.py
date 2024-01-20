578445f9a005c540c19f586575c7c4e22d8e1daf
Andult <dksjj123@gmail.com>
2024-01-20 09:35:40 +0900

    if절 조건추가

--- JY/JY_240120/#1.py
+++ JY/JY_240120/#1.py
@@ -1,9 +1,9 @@
 n=int(input())
-ch=[0]*2
 switch=0
 for i in range(n) :
     ps=input()
     switch=0
+    ch=[0]*2
     for j in range(len(ps)) :
         if ps[j]=='(' :
             ch[0]+=1
@@ -15,7 +15,7 @@ for i in range(n) :
         elif ch[0]<1 and ch[1]>=1 :
             switch=1
             break
-    if switch ==1 :
+    if switch ==1 or ch[0]+ch[1]>0 :
         print('NO')
     elif switch ==0 and ch[0]+ch[1]==0 :
         print('YES')
