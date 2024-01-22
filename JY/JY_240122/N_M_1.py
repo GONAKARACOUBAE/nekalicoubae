eaf460c94e18775fbc8d78786fcaacf5eb787804
Andult <dksjj123@gmail.com>
2024-01-22 10:25:06 +0900

    solved

--- JY/JY_240122/N_M_1.py
+++ JY/JY_240122/N_M_1.py
@@ -1,19 +1,17 @@
 def seq(limit, ea, now_value, now_ea, arr, ch) :
     if now_ea>=ea :
-        for i in range(limit+1) :
-            if ch[i]>1 :
-                return 0
         for i in range(ea) :
             print(a[i], end=" ")
         print()
         return 0
     else :
         for i in range(1, limit+1) :
-            arr.append(i)
-            ch[i]+=1
-            seq(limit, ea, i, now_ea+1, arr, ch)
-            arr.pop()
-            ch[i]-=1
+            if ch[i]==0 :
+                arr.append(i)
+                ch[i]+=1
+                seq(limit, ea, i, now_ea+1, arr, ch)
+                arr.pop()
+                ch[i]-=1
             
 
 m, n = map(int, input().split())
