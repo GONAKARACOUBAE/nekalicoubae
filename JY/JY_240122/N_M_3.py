35654cd85f06d0a72d518cbc1474ed12a06a7c38
Andult <dksjj123@gmail.com>
2024-01-22 09:12:39 +0900

    함수 수정

--- JY/JY_240122/N_M_3.py
+++ JY/JY_240122/N_M_3.py
@@ -4,8 +4,8 @@ def seq(limit, ea, cnt, arr, now_value) :
         seq(limit, ea, cnt+1, arr, now_value)
     elif now_value<=limit :
         print(arr)
-        arr=[]
-        seq(limit, ea, 0, arr, now_value+1)
+        arr.pop()
+        seq(limit, ea, cnt-1, arr, now_value+1)
 m, n = map(int, input().split())
 a=[]
 seq(m, n, 0, a, 1)
