ff1f63059c6e58f2756a5d9ed74f9ac4137b1acb
Andult <dksjj123@gmail.com>
2024-01-22 09:15:15 +0900

    조건 2차수정

--- JY/JY_240122/N_M_3.py
+++ JY/JY_240122/N_M_3.py
@@ -1,8 +1,12 @@
 def seq(limit, ea, cnt, arr, now_value) :
+    if now_value>limit :
+        cnt=0
+        arr=[]
+        seq(limit, ea, cnt, arr, now_value+1)
     if cnt<ea :
         arr.append(now_value)
         seq(limit, ea, cnt+1, arr, now_value)
-    elif now_value<=limit :
+    else :
         print(arr)
         arr.pop()
         seq(limit, ea, cnt-1, arr, now_value+1)
