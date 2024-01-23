62b91a970dbb59cb1e47f388e477b947aab21009
Andult <dksjj123@gmail.com>
2024-01-23 11:31:02 +0900

    배열이 재귀들어가기 전으로 안돌아감
    재귀함수 들어가기 전의 상태로 돌아가야 하는데 이유를 모르겠음.

--- JY/JY_240122/N-QUEEN.py
+++ JY/JY_240122/N-QUEEN.py
@@ -18,18 +18,18 @@ def ch_queen(a, now_x, now_y, bound) :
             a[now_x-i][now_y+i]=1
     return a
 def queen(arr, x, y, num, cnt) :
-    global ans
+    if cnt == 0 :
+        arr = [[0 for j in range(n+1)] for i in range(n+1)]
     if cnt==num :
         ans+=1
     for i in range(1, num+1) :
         for j in range(1, num+1) :
             if arr[i][j]==0 :
-                queen(ch_queen(arr, i, j, num), i, j, num, cnt+1)  
+                arr = ch_queen(arr, i, j, num)
+                queen(arr, i, j, num, cnt+1)
 ans=0
 n=int(input())
 i=0
 j=0
-chess = [[0 for j in range(n+1)] for i in range(n+1)]
-
-queen(chess, 1, 1, n, 0)
+queen(1, 1, 1, n, 0)
 print(ans)
