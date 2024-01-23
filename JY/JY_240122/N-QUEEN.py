3954c5efbe3af6c3d22b97c54a5bd74b26659d4f
Andult <dksjj123@gmail.com>
2024-01-23 20:32:04 +0900

    queen 내부 코드 수정

--- JY/JY_240122/N-QUEEN.py
+++ JY/JY_240122/N-QUEEN.py
@@ -38,21 +38,27 @@ def dch_queen(a, now_x, now_y, bound) :
         if now_x-i>=1 and now_y+i <= bound :
             a[now_x-i][now_y+i]-=1
     return a
-def queen(arr, x, y, num, cnt) :
+def queen(arr, i, j, num, cnt) :
     global ans
-    if cnt == 0 :
-        arr = [[0 for j in range(n+1)] for i in range(n+1)]
     if cnt==num :
         ans+=1
-    for i in range(1, num+1) :
-        for j in range(1, num+1) :
-            if arr[i][j]==0 :
-                arr = ch_queen(arr, i, j, num)
-                queen(arr, i, j, num, cnt+1)
-                arr = dch_queen(arr, i, j, num)
+        return 0
+    while True : 
+        if j+1<=num :
+            j+=1
+        elif j+1> num :
+            i+=1
+            j=1
+        if i>num :
+            break
+        if arr[i][j]==0 :
+            arr = ch_queen(arr, i, j, num)
+            queen(arr, i, j, num, cnt+1)
+            arr = dch_queen(arr, i, j, num)
 ans=0
 n=int(input())
 i=0
 j=0
-queen(1, 1, 1, n, 0)
+chess=[[0 for j in range(n+1)] for i in range(n+1)]
+queen(chess,1, 0, n, 0)
 print(ans)
