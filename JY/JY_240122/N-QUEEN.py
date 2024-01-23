eb9080b822a8a46b94da711243e1f89c0b86faf6
Andult <dksjj123@gmail.com>
2024-01-23 11:53:59 +0900

    풀어주는 작업 및 조건수정

--- JY/JY_240122/N-QUEEN.py
+++ JY/JY_240122/N-QUEEN.py
@@ -1,23 +1,43 @@
 def ch_queen(a, now_x, now_y, bound) :
     for i in range(bound) :
         if now_y+i <=bound :
-            a[now_x][now_y+i]=1
+            a[now_x][now_y+i]+=1
         if now_x+i <=bound :
-            a[now_x+i][now_y]=1
+            a[now_x+i][now_y]+=1
         if now_x-i >=1 :
-            a[now_x-i][now_y]=1
+            a[now_x-i][now_y]+=1
         if now_y-i >=1 :
-            a[now_x][now_y-i]=1
+            a[now_x][now_y-i]+=1
         if now_x+i <=bound and now_y+i <=bound :
-            a[now_x+i][now_y+i]=1
+            a[now_x+i][now_y+i]+=1
         if now_x-i >=1 and now_y-i >=1 :
-            a[now_x-i][now_y-i]=1
+            a[now_x-i][now_y-i]+=1
         if now_x+i <= bound and now_y-i >=1 :
-            a[now_x+i][now_y-i]=1
+            a[now_x+i][now_y-i]+=1
         if now_x-i>=1 and now_y+i <= bound :
-            a[now_x-i][now_y+i]=1
+            a[now_x-i][now_y+i]+=1
+    return a
+def dch_queen(a, now_x, now_y, bound) :
+    for i in range(bound) :
+        if now_y+i <=bound :
+            a[now_x][now_y+i]-=1
+        if now_x+i <=bound :
+            a[now_x+i][now_y]-=1
+        if now_x-i >=1 :
+            a[now_x-i][now_y]-=1
+        if now_y-i >=1 :
+            a[now_x][now_y-i]-=1
+        if now_x+i <=bound and now_y+i <=bound :
+            a[now_x+i][now_y+i]-=1
+        if now_x-i >=1 and now_y-i >=1 :
+            a[now_x-i][now_y-i]-=1
+        if now_x+i <= bound and now_y-i >=1 :
+            a[now_x+i][now_y-i]-=1
+        if now_x-i>=1 and now_y+i <= bound :
+            a[now_x-i][now_y+i]-=1
     return a
 def queen(arr, x, y, num, cnt) :
+    global ans
     if cnt == 0 :
         arr = [[0 for j in range(n+1)] for i in range(n+1)]
     if cnt==num :
@@ -27,6 +47,7 @@ def queen(arr, x, y, num, cnt) :
             if arr[i][j]==0 :
                 arr = ch_queen(arr, i, j, num)
                 queen(arr, i, j, num, cnt+1)
+                arr = dch_queen(arr, i, j, num)
 ans=0
 n=int(input())
 i=0
