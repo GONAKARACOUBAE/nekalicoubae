9e13edbe9dfdcc334e419bed87d5bac96a6f8e50
yeongwoo <ywoo12321@gmail.com>
2024-01-23 16:53:46 +0900

    [FEAT] problem1 solve

--- YW/240122/NM1.js
+++ YW/240122/NM1.js
@@ -1,10 +1,12 @@
-// const input = require("fs").readFileSync("/dev/stdin").toString().split("\n").slice(0, -1);
+const fs = require("fs");
+// const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+
+// const input = fs.readFileSync("/dev/stdin").toString().split("\n").slice(0, -1);
 // const NM = input[0].split(" ");
 // const [n, m] = NM.map((el) => Number(el));
 
-const n = 3;
-const m = 2;
-
+const n = 4;
+const m = 4;
 let answer = "";
 let selected = [];
 let a = "";
@@ -14,13 +16,14 @@ const solution = (number) => {
     return;
   } else {
     for (let cand = 1; cand <= n; cand++) {
-      selected[number] = cand;
-      solution(number + 1);
-      selected[number] = 0;
+      if (!selected.includes(cand)) {
+        selected[number] = cand;
+        solution(number + 1);
+        selected[number] = 0;
+      }
     }
   }
 };
 
 solution(0);
-a = JSON.stringify(answer);
 console.log(answer);
