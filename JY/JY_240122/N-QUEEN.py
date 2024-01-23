fe20e520cae58832d9d43ef024c1cae93b62b9cb
Andult <dksjj123@gmail.com>
2024-01-23 10:51:42 +0900

    조건 및 함수 수정

--- JY/JY_240122/N-QUEEN.py
+++ JY/JY_240122/N-QUEEN.py
@@ -12,17 +12,20 @@ def ch_queen(a, now_x, now_y, bound) :
             a[now_x+i][now_y+i]=1
         if now_x-i >=1 and now_y-i >=1 :
             a[now_x-i][now_y-i]=1
+        if now_x+i <= bound and now_y-i >=1 :
+            a[now_x+i][now_y-i]=1
+        if now_x-i>=1 and now_y+i <= bound :
+            a[now_x-i][now_y+i]=1
     return a
 def queen(arr, x, y, num, cnt) :
     global ans
     if cnt==num :
         ans+=1
-    if arr[x][y]==0 :
-        arr=ch_queen(arr, x, y, num)
-    if y+1 <= num :
-        queen(arr,x,y+1,num, cnt+1)
-    elif x+1 <= num :
-        queen(arr, x+1, 1, num, cnt+1)    
+    for i in range(1, num+1) :
+        for j in range(1, num+1) :
+            if arr[i][j]==0 :
+                arr=ch_queen(arr, i, j, num)
+                queen(arr, i, j, num, cnt+1)  
 ans=0
 n=int(input())
 i=0
