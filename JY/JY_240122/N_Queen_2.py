b2f1d08e53a4509cb26f48e92e718385238f513b
Andult <dksjj123@gmail.com>
2024-01-23 23:16:35 +0900

    새로운 방식. 하지만 시간초과

--- JY/JY_240122/N_Queen_2.py
+++ JY/JY_240122/N_Queen_2.py
@@ -11,28 +11,26 @@ def ch_queen (ch, x, y, cnt) :
     return 1
 def queen(ch, i, j, cnt, num, switch) :
     global ans
-    if switch==0 :
-        if cnt == num :
-            ans+=1
-            switch=1
-            return 0
-        else :
-            while True : 
-                if j<num :
-                    j+=1
-                if j>=num :
-                    i+=1
-                    j=0
-                if i==num :
-                    break
-                if ch_queen(ch, i, j, cnt) == 1 :
-                    ch[cnt][0]=i
-                    ch[cnt][1]=j
-                    queen(ch, i, j, cnt+1, num, 0)
-                    ch[cnt][0]=None
-                    ch[cnt][1]=None
+    if cnt == num :
+        ans+=1
+        return 0
+    else :
+        while True : 
+            if j<num :
+                j+=1
+            if j>=num :
+                i+=1
+                j=0
+            if i==num :
+                break
+            if ch_queen(ch, i, j, cnt) == 1 :
+                ch[cnt][0]=i
+                ch[cnt][1]=j
+                queen(ch, i, j, cnt+1, num)
+                ch[cnt][0]=None
+                ch[cnt][1]=None
 n=int(input())
 ans=0
 ch=[[None for j in range(2)] for i in range(n)]
-queen(ch, 0, -1, 0, n, 0)
+queen(ch, 0, -1, 0, n)
 print(ans)
