dabfe73fa035c303782cd5e0a3a13b590e26727e
Andult <dksjj123@gmail.com>
2024-01-24 10:01:09 +0900

    자음조건 추가

--- JY/JY_240124/password.py
+++ JY/JY_240124/password.py
@@ -2,9 +2,9 @@ def ch_vowels(alpha) :
     if alpha == 'a' or alpha == 'e' or alpha == 'i' or alpha == 'o' or alpha == 'u' :
         return 1
     return 0
-def ch_pass(l, c, al_list, arr, ea, j, vowels) :
+def ch_pass(l, c, al_list, arr, ea, j, vowels, constants) :
     if ea == l :
-        if vowels == 1 :
+        if vowels == 1 and constants >=2 :
             for i in range (l) :
                 print(arr[i], end="")
             print()
@@ -13,11 +13,14 @@ def ch_pass(l, c, al_list, arr, ea, j, vowels) :
             arr.append(al_list[i])
             if ch_vowels(al_list[i]) == 1 :
                 vowels = 1
-            ch_pass(l, c, al_list, arr, ea+1, i+1, vowels)
+            else :
+                constants+=1
+            ch_pass(l, c, al_list, arr, ea+1, i+1, vowels, constants)
             arr.pop()
+            if ch_vowels(al_list[i]) == 0 :
+                constants-=1
 l, c = map(int, input().split())
 al_list = input().split()
 al_list.sort()
 arr=[]
-ch=[0]*180
-ch_pass(l, c, al_list, arr, 0, 0, 0)
+ch_pass(l, c, al_list, arr, 0, 0, 0, 0)
