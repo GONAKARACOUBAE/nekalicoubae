416ab807acc22cdd509ef06cc990aa31a4d295e9
Andult <dksjj123@gmail.com>
2024-01-24 09:52:47 +0900

    출력 수정

--- JY/JY_240124/password.py
+++ JY/JY_240124/password.py
@@ -2,23 +2,22 @@ def ch_vowels(alpha) :
     if alpha == 'a' or alpha == 'e' or alpha == 'i' or alpha == 'o' or alpha == 'u' :
         return 1
     return 0
-def ch_pass(l, c, al_list, arr, ea, ch, vowels) :
+def ch_pass(l, c, al_list, arr, ea, j, vowels) :
     if ea == l :
         if vowels == 1 :
-            print(arr)
+            for i in range (l) :
+                print(arr[i], end="")
+            print()
     else :
-        for i in range(c) :
-            if ch[ord(al_list[i])] == 0 :
-                arr.append(al_list[i])
-                ch[ord(al_list[i])]=1
-                if ch_vowels(al_list[i]) == 1 :
-                    vowels = 1
-                ch_pass(l, c, al_list, arr, ea+1, ch, vowels)
-                arr.pop()
-                ch[ord(al_list[i])]=0
+        for i in range(j, c) :
+            arr.append(al_list[i])
+            if ch_vowels(al_list[i]) == 1 :
+                vowels = 1
+            ch_pass(l, c, al_list, arr, ea+1, i+1, vowels)
+            arr.pop()
 l, c = map(int, input().split())
 al_list = input().split()
 al_list.sort()
 arr=[]
 ch=[0]*180
-ch_pass(l, c, al_list, arr, 0, ch, 0)
+ch_pass(l, c, al_list, arr, 0, 0, 0)
