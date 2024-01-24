96a6f96cf3e359b9f390f6fd9bfa5d82cb4112a3
Andult <dksjj123@gmail.com>
2024-01-24 09:46:46 +0900

    문제 잘못 이해함

--- JY/JY_240124/password.py
+++ JY/JY_240124/password.py
@@ -1,7 +1,24 @@
-def ch_pass(l, c, arr) :
+def ch_vowels(alpha) :
+    if alpha == 'a' or alpha == 'e' or alpha == 'i' or alpha == 'o' or alpha == 'u' :
+        return 1
     return 0
+def ch_pass(l, c, al_list, arr, ea, ch, vowels) :
+    if ea == l :
+        if vowels == 1 :
+            print(arr)
+    else :
+        for i in range(c) :
+            if ch[ord(al_list[i])] == 0 :
+                arr.append(al_list[i])
+                ch[ord(al_list[i])]=1
+                if ch_vowels(al_list[i]) == 1 :
+                    vowels = 1
+                ch_pass(l, c, al_list, arr, ea+1, ch, vowels)
+                arr.pop()
+                ch[ord(al_list[i])]=0
 l, c = map(int, input().split())
 al_list = input().split()
 al_list.sort()
-print(al_list)
-ch_pass(l, c, al_list)
+arr=[]
+ch=[0]*180
+ch_pass(l, c, al_list, arr, 0, ch, 0)
