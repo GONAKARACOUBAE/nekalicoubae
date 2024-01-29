433664e43982f4aca939214b026ae305a9964f40
Andult <dksjj123@gmail.com>
2024-01-29 11:48:26 +0900

    다중조건으로 정렬

--- JY/JY_240129/card2.py
+++ JY/JY_240129/card2.py
@@ -4,10 +4,10 @@ ans={}
 
 n=int(sys.stdin.readline())
 for i in range(n) :
-    num=input()
+    num=int(input())
     if (num in ans) == False :
         ans[num]=1
     else :
         ans[num]+=+1
-sorted_ans = sorted(ans.items(), key = lambda item:item[1], reverse=True)
+sorted_ans = sorted(ans.items(), key = lambda item: (-item[1], item[0]))
 print(sorted_ans[0][0])
