8ecdb57239f64c24164b0a7695b98d1e19d13ec8
Andult <dksjj123@gmail.com>
2024-01-29 21:34:02 +0900

    ctrl+D 어떻게하는거지;

--- JY/JY_240129/arrow.py
+++ JY/JY_240129/arrow.py
@@ -5,4 +5,18 @@ arr = defaultdict(list)
 for i in range(n) :
     a, b = map(int, sys.stdin.readline().split())
     arr[b].append(a)
-print(arr)
+ans = 0
+sorted_arr = sorted(arr.items(), key = lambda item: item[1])
+
+for i in arr.keys() :
+    for j in range(len(arr[i])) :
+        if j == 0 :
+            ans+=arr[i][1]-arr[i][0]
+        elif j==len(arr[i])-1 :
+            ans+=arr[i][len(arr[i])-1]-arr[i][len(arr[i])-2]
+        else :
+            if arr[i][j]-arr[i][j-1] > arr[i][j+1]-arr[i][j] :
+                ans+=arr[i][j+1]-arr[i][j]
+            else :
+                ans+=arr[i][j]-arr[i][j-1]
+print(ans)
