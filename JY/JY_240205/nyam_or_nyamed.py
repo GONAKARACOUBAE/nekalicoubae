a4bb1022e0f13447a1c4f97f6826437c8f0f1781
Andult <dksjj123@gmail.com>
2024-02-05 09:14:40 +0900

    입력받는거 복습하기. nyam 5번째줄

---
+++ JY/JY_240205/nyam_or_nyamed.py
@@ -0,0 +1,6 @@
+import sys
+t=int(sys.stdin.readline())
+for i in range(t) :
+    n, m=map(int,sys.stdin.readline().split())
+    arr_n=list(map(int, sys.stdin.readline().split()))
+    print(arr_n)
