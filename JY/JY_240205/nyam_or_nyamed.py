90de236e63f66eba1ac07976adb4f806108b0ff6
Andult <dksjj123@gmail.com>
2024-02-05 10:19:50 +0900

    초안, 이진탐색이 이게 맞나도 잘 모르겠고 구현도 어렵다

--- JY/JY_240205/nyam_or_nyamed.py
+++ JY/JY_240205/nyam_or_nyamed.py
@@ -1,6 +1,23 @@
 import sys
 t=int(sys.stdin.readline())
+dap=[]
 for i in range(t) :
     n, m=map(int,sys.stdin.readline().split())
-    arr_n=list(map(int, sys.stdin.readline().split()))
-    print(arr_n)
+    arr_a=list(map(int, sys.stdin.readline().split()))
+    arr_b=list(map(int, sys.stdin.readline().split()))
+    arr_a.sort()
+    arr_b.sort()
+    ans=0
+    for j in arr_b :
+        left=0
+        right=n-1
+        while left!=right :
+            idx = int((left + right + 1) /2)
+            if arr_a[idx] <= j :
+                left=idx
+            elif arr_a[idx] >= j :
+                right=idx-1
+        ans+=n-left-1
+    dap.append(ans)
+for i in dap :
+    print(i)
