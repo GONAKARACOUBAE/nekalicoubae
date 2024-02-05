7c64565865e4926bf7bc457bf6c66df66574c15c
Andult <dksjj123@gmail.com>
2024-02-05 23:13:12 +0900

    이게 이진탐색으로 도대체 어떻게 되는건지 답지 체크해보자

--- JY/JY_240205/nyam_or_nyamed.py
+++ JY/JY_240205/nyam_or_nyamed.py
@@ -1,4 +1,15 @@
 import sys
+def bin(start, end, target) :
+    mid = int((start + end)/2)
+    if start>end :
+        return 0
+    if arr_a[mid]==target :
+        return mid
+    elif arr_a[mid]>target :
+        return bin(start, mid-1, target)
+    else :
+        return bin(mid+1, end, target)
+
 t=int(sys.stdin.readline())
 dap=[]
 for i in range(t) :
@@ -6,19 +17,9 @@ for i in range(t) :
     arr_a=list(map(int, sys.stdin.readline().split()))
     arr_b=list(map(int, sys.stdin.readline().split()))
     arr_a.sort()
-    arr_b.sort()
     ans=0
     for j in arr_b :
-        left=0
-        right=n-1
-        while left!=right :
-            idx = int((left + right + 1) /2)
-            if arr_a[idx] <= j :
-                left=idx
-            elif arr_a[idx] >= j :
-                right=idx-1
-        ans+=n-left-1
+        ans+=(n-bin(0, n-1, j)-1)
     dap.append(ans)
 for i in dap :
     print(i)
-so hard
