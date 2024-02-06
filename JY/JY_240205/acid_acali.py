7ac48090599fc7f36061532eeabc4953dacca9a1
Andult <dksjj123@gmail.com>
2024-02-06 20:43:16 +0900

    recursion error

---
+++ JY/JY_240205/acid_acali.py
@@ -0,0 +1,16 @@
+import sys
+def search(low, high, min) :
+    if low>=high :
+        return 0
+    if abs(arr[low]+arr[high]) < min :
+        ans[0]=arr[low]
+        ans[1]=arr[high]
+        min=abs(arr[low]+arr[high])
+    search(low+1, high, min)
+    search(low, high-1, min)
+n=int(sys.stdin.readline())
+arr=list(map(int, sys.stdin.readline().split()))
+arr.sort()
+ans=[0, 0]
+search(0, n-1, sys.maxsize)
+print(ans[0], ans[1])
