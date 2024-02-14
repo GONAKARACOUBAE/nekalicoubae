f60732f94795811702eb04ce4cde788a53d5469f
Andult <dksjj123@gmail.com>
2024-02-14 22:52:01 +0900

    solved

--- JY/JY_240214/select_num.py
+++ JY/JY_240214/select_num.py
@@ -5,17 +5,21 @@ for i in range(n) :
     arr.append(int(sys.stdin.readline().rstrip()))
 arr.sort()
 low=0
-high=len(arr)-1
+high=1
 ans=sys.maxsize
 while True :
-    temp=arr[high]-arr[low]
-    if low >= high or temp < m :
+    if  high==n :
         break
-    if temp >= m and temp < ans :
+    temp=arr[high]-arr[low]
+    if temp >= m and ans>temp :
         ans=temp
-    if arr[high]-arr[low+1] < arr[high-1]-arr[low] :
+        if temp==m :
+            break
+    if temp > m :
         low+=1
+        high=low+1
     else :
-        high-=1
+        high+=1
+
 print(ans)
     
