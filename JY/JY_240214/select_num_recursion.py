94497e78706c9314f682e3092b932786d02353a2
Andult <dksjj123@gmail.com>
2024-02-14 22:20:51 +0900

    자꾸 완전탐색으로 간다.

---
+++ JY/JY_240214/select_num_recursion.py
@@ -0,0 +1,22 @@
+import sys
+def search(low, high) :
+    global ans
+    global m
+    temp=arr[high]-arr[low]
+    if low > high or temp < m :
+        return 0
+    if temp >= m and temp < ans :
+        ans=temp
+    search(low+1, high)
+    search(low,high-1)
+n, m = map(int, sys.stdin.readline().split())
+arr=[]
+for i in range(n) :
+    arr.append(int(sys.stdin.readline().rstrip()))
+arr.sort()
+low=0
+high=len(arr)-1
+ans=sys.maxsize
+search(low, high)
+print(ans)
+    
