a547a2bb8c683ce2ee85f03f6b3430c6198b2fcd
Andult <dksjj123@gmail.com>
2024-02-14 11:19:40 +0900

    재귀가 더 오래걸리네?

---
+++ JY/JY_240214/sumarr copy.py
@@ -0,0 +1,26 @@
+import sys
+sys.setrecursionlimit(10**9)
+def sumarr(x, y, arr) :
+    if y==m :
+        for i in range(x, n) :
+            arr.append(a[i])
+        return arr
+    elif x==n :
+        for i in range(y, m) :
+            arr.append(b[i])
+        return arr
+    if a[x]<b[y] :
+        arr.append(a[x])
+        return sumarr(x+1, y, arr)
+    else :
+        arr.append(b[y])
+        return sumarr(x, y+1, arr)
+n, m = map(int, sys.stdin.readline().split())
+a = list(map(int, sys.stdin.readline().split()))
+b = list(map(int, sys.stdin.readline().split()))
+a.sort()
+b.sort()
+ans=[]
+ans=sumarr(0,0,ans)
+for i in range(n+m) :
+    print(ans[i], end=' ')
