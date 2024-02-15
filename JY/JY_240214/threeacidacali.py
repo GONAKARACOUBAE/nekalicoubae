acf3727e2f9192789f170a2c2d781564bc119fe7
Andult <dksjj123@gmail.com>
2024-02-15 09:56:47 +0900

    solved

--- JY/JY_240214/threeacidacali.py
+++ JY/JY_240214/threeacidacali.py
@@ -16,8 +16,6 @@ for i in range(n-2) :
             res[2]=arr[high]
     else :
         while True :
-            if high==i :
-                high-=1
             if low==high :
                 break
             hap = arr[low]+arr[high]+arr[i]
@@ -28,8 +26,13 @@ for i in range(n-2) :
                 res[2]=arr[high]
             if hap > 0 :
                 high-=1
-            else :
+            elif hap < 0 :
                 low+=1
+            else :
+                res[0]=arr[i]
+                res[1]=arr[low]
+                res[2]=arr[high]
+                break
 for i in range(3) :
     print(res[i], end=' ')
             
