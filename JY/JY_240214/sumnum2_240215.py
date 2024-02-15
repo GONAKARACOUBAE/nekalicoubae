fe9a79da92244c7048d6a1b343cffc3e9e86cc25
Andult <dksjj123@gmail.com>
2024-02-15 10:22:54 +0900

    solved

---
+++ JY/JY_240214/sumnum2_240215.py
@@ -0,0 +1,33 @@
+import sys
+n, m = map(int, sys.stdin.readline().split())
+a=list(map(int, sys.stdin.readline().split()))
+low=high=0
+hap=a[low]
+ans=0
+while True :
+    if low>high :
+        high=low
+        hap=a[low]
+    if hap == m :
+        ans+=1
+        high+=1
+        if high!=n :
+            hap+=a[high]
+        else :
+            hap-=a[low]
+            low+=1
+            if hap < m :
+                break
+    elif hap>m :
+        hap-=a[low]
+        low+=1
+    else :
+        high+=1
+        if high!=n :
+            hap+=a[high]
+        else :
+            hap-=a[low]
+            low+=1
+            if hap < m :
+                break
+print(ans)
