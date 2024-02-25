c9b763742a334137d9218ad52e3580cfc4dfa5fd
Andult <104719742+AJY3124@users.noreply.github.com>
2024-02-25 20:21:15 +0900

    low가 문제인가?

--- JY/JY_240225/money.py
+++ JY/JY_240225/money.py
@@ -4,6 +4,7 @@ arr=list(map(int, sys.stdin.readline().split()))
 m=int(sys.stdin.readline())
 low=min(arr)
 high=max(arr)
+ans=0
 while True :
     mid=(low+high)//2
     hap=0
@@ -12,10 +13,10 @@ while True :
             hap+=i
         else :
             hap+=mid
-    if hap>=m :
-        ans=hap
+    if hap>m :
         high=mid-1
     else :
+        ans=mid
         low=mid+1
     if low > high :
         break
